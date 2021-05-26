8bd266596e80c0b2c2a9293e657b499adee96d49
ristokynnapas <ristokynnapas@gmail.com>
2021-05-26 07:03:39 +0300

    26.05.2021 changes: added main.py, model_architecture.ipynb etc.

---
+++ Data collection/video_generator.py
@@ -0,0 +1,24 @@
+import glob
+import cv2 as cv
+import collections
+
+image_array = collections.deque([])
+for image_file in glob.glob("C:/Users/Personal/Desktop/WindowsNoEditor/PythonAPI/examples/_out/*.png"):
+    image = cv.imread(image_file)
+    image_array.append(image)
+
+# the size of the images should be the same (last image size) ->
+height, width, layers = image.shape
+size = (width,height)
+
+# video codec and params ->
+result = cv.VideoWriter("Result videos/Proof_of_concept1.avi", cv.VideoWriter_fourcc(*"MJPG"), 10, size)
+
+# generating frame-by-frame the video file in result variable -> 
+for frame in range(len(image_array)):
+    result.write(image_array[frame])
+
+# when done, exit safely ->
+cv.destroyAllWindows()
+result.release()
+
